0463967d3ef398d985d7e371491bfe5b982725c5
ravikanthreddy <ravikanth.ravi456@gmail.com>
2026-03-14 00:08:28 +0530

    conversation variable added

---
+++ 03-variabel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=RAMESH
+PERSON2=SURESH
+
+
+echo "$PERSON1:: hi $PERSON2"
+echo " $PERSON2:: hi $PERSON1"
+echo "$PERSON1:: how are you doing" 
+echo "$PERSON2:: iam good how are you"
