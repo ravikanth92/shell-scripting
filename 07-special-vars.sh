f7856a6b7931295e227712ba08acac6212be51e5
ravikanthreddy <ravikanth.ravi456@gmail.com>
2026-03-17 10:39:32 +0530

    special variables passed here

---
+++ 07-special-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passed: $@"
+echo "Number of variables: $#"
+echo "script name: $0"
+echo "present working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Process id of current script: $$"
+sleep 60
+echo "Process id of last command in backround: $!"
